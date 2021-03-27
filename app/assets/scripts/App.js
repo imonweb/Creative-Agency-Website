8a8444fbd92e6ce43d86c3772f7b117f635bf244
Imon <contact@imonweb.co.uk>
2021-03-27 01:34:37 +0000

    js modified

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,3 +6,16 @@ let menu = new Menu();
 if(module.hot){
   module.hot.accept()
 }
+
+/*  Toggle Menu */
+
+/*
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
+
+*/
